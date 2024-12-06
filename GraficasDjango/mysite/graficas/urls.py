f3055c6e0556d6c55f9b632167648ab515991960
Raam156 <dadaw848@gmail.com>
2024-12-05 22:32:01 -0600

    Update

--- GraficasDjango/mysite/graficas/urls.py
+++ GraficasDjango/mysite/graficas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Ajusta la vista según tu implementación
+    path('', views.index, name='index'),  
 ]
