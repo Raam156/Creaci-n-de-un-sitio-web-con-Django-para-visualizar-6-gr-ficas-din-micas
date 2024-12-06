5a354cda810e2c07c1442556e1b876df5ab1e9f5
Raam156 <dadaw848@gmail.com>
2024-12-05 22:33:05 -0600

    Update urls.py

--- GraficasDjango/mysite/mysite/urls.py
+++ GraficasDjango/mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('graficas.urls')),  # Esto incluye las URLs de la app "graficas"
+    path('', include('graficas.urls')),  
 ]
